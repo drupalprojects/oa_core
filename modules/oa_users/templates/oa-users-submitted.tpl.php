a6bc95dfa4fda823d655cfe4b379ec44e768902d
Mike Potter <mpotter@phase2technology.com>
2016-02-01 11:19:19 -0500

    Add image style options to user submitted and add option to hide page title

--- modules/oa_users/templates/oa-users-submitted.tpl.php
+++ modules/oa_users/templates/oa-users-submitted.tpl.php
@@ -1,8 +1,9 @@
+<?php if ($show_title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
 <div class="oa-users-submitted align-<?php print $align;?>">
   <?php if ($align == 'left'):?>
     <div class="user-badge">
       <?php if (!empty($picture)):?>
-        <?php print $picture; ?>
+        <span class="oa-user-picture"><?php print $picture; ?></span>
       <?php endif; ?>
       <?php if (!empty($userlink)):?>
         <span><?php print $userlink; ?></span>
@@ -26,7 +27,7 @@
         <span><?php print $userlink; ?></span>
       <?php endif; ?>
       <?php if (!empty($picture)):?>
-        <?php print $picture; ?>
+        <span class="oa-user-picture"><?php print $picture; ?></span>
       <?php endif; ?>
     </div>
   <?php endif; ?>
